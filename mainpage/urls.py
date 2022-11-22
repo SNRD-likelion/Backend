91473d92c5c9bc31f1911f64db741dd6d2acb52a
AhnYoungHoon <dksdudgns312@naver.com>
2022-11-22 19:00:03 +0900

    mainpage api, url변경

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:project_id>/', views.allData), # GET 메인페이지(원페이저) 처음 랜딩될 때 데이터전체 전달
-    # path('<int:project_id>/change', views.change),
+    path('<int:project_id>/change', views.orderChange),
 ]
