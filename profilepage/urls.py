fef384c10f816fabfab17c1d165fc888f8d4f4ce
AhnYoungHoon <dksdudgns312@naver.com>
2022-12-15 16:19:12 +0900

    runtime 추가

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<email>/create/', views.createProject), # 프로젝트 생성
-    path('<email>/position/', views.editPostion), # 각 프로젝트별 역할 수정
-    path('<email>/information/', views.editIntro), # 각 프로젝트별 역할 수정
+    path('<email>/create', views.createProject), # 프로젝트 생성
+    path('<email>/position', views.editPostion), # 각 프로젝트별 역할 수정
+    path('<email>/information', views.editIntro), #  소개 수정
     path('<email>/', views.allData), # 처음 프로젝트 띄울 때 프로젝트들 정보넘겨주기
 ]
