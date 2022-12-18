427dda5cb7046628fd45d82e45012ee8073c466f
AhnYoungHoon <dksdudgns312@naver.com>
2022-12-18 19:20:45 +0900

    utf8 setting

--- sunaropdaback/settings.py
+++ sunaropdaback/settings.py
@@ -134,7 +134,8 @@ DATABASES = {
         'HOST': RDS_HOST,
         'PORT': RDS_PORT,
         'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset' : 'utf8mb4'
         }
     }
 }
